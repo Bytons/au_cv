6913379275f92cdc8cff4424ddf88690998b083f
Antti Häkkinen <antti.hakkinen94@gmail.com>
2019-02-03 18:18:14 +0200

    prod build v2

---
+++ dist/precache-manifest.060351010707375abbefe3d56363af0e.js
@@ -0,0 +1,322 @@
+self.__precacheManifest = [
+  {
+    "url": "/app~d0ae3f07.3452ad359ed7e82de174.chunk.js"
+  },
+  {
+    "url": "/runtime~app.4fd41a1598857eb299e1.bundle.js"
+  },
+  {
+    "url": "/vendor.aurelia-binding~4157a4ee.b30c32dea3b51ad3b0ff.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-dependency-injection~fbc292be.6bc9ebac7730c90fb987.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-framework~b33c7998.91f0e260f036e10e97b1.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-loader-webpack~6926fbb5.54d9782cef7933fadf48.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~0cd1d67a.a1b32cd11a5a24298e07.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~3c9044e3.333e9ce408502ee619de.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~49af55f3.2554e1361649542b5216.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~67666563.b7bfe89daf767201db8d.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~67875f29.1e862907c5477875d916.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~80545748.c74e483155bfc6cd2973.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~a99f1de4.61631d2a5d28307d90c6.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~ab64e055.cfd26d7c88d110608c0d.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~ae085e67.a621375b4289f38cd720.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~b19764ad.8de0a0447d1fd1b326ce.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-materialize-bridge~bbcbdea2.812f763f9e9b85398c28.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-pal-browser~d6c0e73b.13a6511de7b81ae28e86.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-polyfills~dc8a991d.3604cbe13307dfa40f4d.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-route-recognizer~8134d493.79fe81356c906c01828e.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-router~ae38da23.74f90f97a51797f2b60a.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-templating-binding~6483373c.2799d0fe2b314d443422.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-templating-resources~25ed4a20.0fae76e4a36a8440379a.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-templating-resources~ef465739.b9c5d6ec7dc13f16a388.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-templating-resources~fdb35320.5972084cc5ca84bc9a25.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-templating-router~1e71eeae.53be7c802ea83c84772e.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-templating~9e0f4621.9ef0e4a599a5835a719d.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-testing~4eb3b4b8.b5b6c85f16aa8951e709.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-typed-observable-plugin~d03cd92b.925cf8f58adeb12d1fd0.chunk.js"
+  },
+  {
+    "url": "/vendor.aurelia-validation~6903c527.2da41eeca4dbb0bca3f0.chunk.js"
+  },
+  {
+    "url": "/vendor.bluebird~01be3b92.a82a71ed93337060f19c.chunk.js"
+  },
+  {
+    "url": "/vendor.materialize-css~49dbf828.c767aa1be7ff9527a73e.chunk.js"
+  },
+  {
+    "url": "/vendor.materialize-css~8e6971e1.9e91cdc41e69d24132b1.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~0a56fd24.223f000a262c2c40e7c2.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~13aac730.395644f5fdf2ef3cd084.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~15f0789d.ea3415bba8105a54e66e.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~1d8d93f8.0376bddb6946264e9fbe.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~1e23895a.35865fe59da22bb02cb4.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~31c708a5.a0fa81299beedbd3817e.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~3867e4ef.6f3278c05fca8bd85d00.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~399b027d.856608e7656de518f5c3.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~5f43c043.53181b7d79faa6945cbc.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~679e2cd8.ac5d030fd9d831a56a62.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~7db804d5.5ded717191dbb539f572.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~8a7b4606.f360f9fd0ae7433f2e42.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~8c8f66f3.6704136b2ec41c079548.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~8eeb4602.d034742e06bd8e803e8d.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~91884273.1ae446bd27fe8c13dbf3.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~a884eab2.08619e470cbbadbe4167.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~b5b59692.eeafef3f067ebe857cd5.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~b895b8ba.d5fdecf90557393d9500.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~bc6e0817.312a6c56db17c39a702b.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~cc99a214.4cdb1e51d7da112d303b.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~e258e298.6a13dbf67cd519edaad8.chunk.js"
+  },
+  {
+    "url": "/vendor.moment~e42b2e82.1747e5d0a319c914bb07.chunk.js"
+  },
+  {
+    "url": "/vendor.style-loader~96c663eb.3beec21df2526cfb65b4.chunk.js"
+  },
+  {
+    "url": "/vendors~2a42e354.3d5b22e4c331574cd4c0.chunk.js"
+  },
+  {
+    "url": "/vendors~4c12d43a.ab8de57e5dfb8f3f96f0.chunk.js"
+  },
+  {
+    "url": "/vendors~ec8c427e.68a9b788332d8a25b4a0.chunk.js"
+  },
+  {
+    "url": "/eb9c6ba81ad9c71b985d0563c5fde71e.jpg"
+  },
+  {
+    "revision": "5b077aafa4573b8e042ed75a2cfe674d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb9c6ba81ad9c71b985d0563c5fde71e",
+    "url": "/images/watoo.jpg"
+  },
+  {
+    "revision": "728ff5a8e44d74cd0f2359ef0a9ec88a",
+    "url": "/images/logos/git-logo.png"
+  },
+  {
+    "revision": "9663a554592d99a69510c9a4120adfa5",
+    "url": "/images/logos/gradle-logo.svg"
+  },
+  {
+    "revision": "0cb24d84cf8ceaec26d1f62fb7d45d42",
+    "url": "/images/logos/grails_logo.svg"
+  },
+  {
+    "revision": "a34c4dcaf2ffc944ef13ed14683dded2",
+    "url": "/images/logos/html5-logo.png"
+  },
+  {
+    "revision": "1c51d4234f9e91e7b91c8a3169bbd571",
+    "url": "/images/logos/mariadb-logo.png"
+  },
+  {
+    "revision": "97988de920ff6f99e3d45b73022245fd",
+    "url": "/images/logos/notepad-logo.png"
+  },
+  {
+    "revision": "91a7c76b02b5442b7e2c3b1d1b3ea250",
+    "url": "/images/logos/npm-logo.png"
+  },
+  {
+    "revision": "534f942fe4d57dc8f7cc3b9f2a657bf1",
+    "url": "/images/logos/spring-logo.png"
+  },
+  {
+    "revision": "8258c5b212fa4c5777f33254afcb3b24",
+    "url": "/images/logos/worker-logo.svg"
+  },
+  {
+    "revision": "e8c27f6b0fcf596a06a31135d937c73e",
+    "url": "/images/logos/JS-logo.png"
+  },
+  {
+    "revision": "865c7e1c234dffcb63f583e84d176a44",
+    "url": "/images/logos/user-design-logo.svg"
+  },
+  {
+    "revision": "0261db2fe867bd10fa2491abb95bbae0",
+    "url": "/images/logos/vscode-logo.png"
+  },
+  {
+    "revision": "40d7bd3af16f1594c9687f368a4c0323",
+    "url": "/images/oneclick.png"
+  },
+  {
+    "revision": "2496b0337235a7c422f56e779ef50e03",
+    "url": "/images/tiira.png"
+  },
+  {
+    "revision": "ce6259d2e1e01933a88d237690ba1077",
+    "url": "/images/logos/maven-logo.png"
+  },
+  {
+    "revision": "a69f63a2f569aebc8a7e13514dc80c4b",
+    "url": "/images/logos/pwa-logo.png"
+  },
+  {
+    "revision": "26cc95f255ccb936d154b43614f61602",
+    "url": "/images/logos/ts-logo.png"
+  },
+  {
+    "revision": "4c88e46e01a109870a81d0da5a87f15a",
+    "url": "/images/birdlife_logo.png"
+  },
+  {
+    "revision": "a717eafa290bf333c4dd1c86076c5b9e",
+    "url": "/images/logos/node-logo.png"
+  },
+  {
+    "revision": "04cf122be0afe7542083d1a3967390ae",
+    "url": "/images/logos/css-logo.png"
+  },
+  {
+    "revision": "61c3c0c02e69684302d18c5beb76462b",
+    "url": "/images/logos/spa-logo.png"
+  },
+  {
+    "revision": "f7056bf67f49a8c4f0293076919f7c16",
+    "url": "/images/maarianvaara.jpg"
+  },
+  {
+    "revision": "4c25acb49507a6be7d2e1e91b0ffa2a5",
+    "url": "/images/logos/intelliJ-logo.png"
+  },
+  {
+    "revision": "b22d9a7a57ccd7d1d0eb5821b5c9ef0b",
+    "url": "/images/logos/groovy-logo.png"
+  },
+  {
+    "revision": "f8bdc67cd7b3728b7fcd8427d8360efc",
+    "url": "/images/logos/java-logo.png"
+  },
+  {
+    "revision": "2177a3e30b8d41c9f3e3cd76fee08e1a",
+    "url": "/images/antti.jpg"
+  },
+  {
+    "revision": "812c54906689b0dca03463f91c5b81d0",
+    "url": "/images/logos/aurelia-logo.jpg"
+  },
+  {
+    "revision": "cde43891991f76a5e7df17ac6d373aff",
+    "url": "/images/logos/mySQL-logo.png"
+  },
+  {
+    "revision": "73e24896289a49f3365faf66faec2f65",
+    "url": "/images/logos/sass-logo.png"
+  },
+  {
+    "revision": "0e23481b805fa66eb9ff0c177ff27030",
+    "url": "/images/logos/mongodb-logo.png"
+  },
+  {
+    "revision": "bed10082058c5def8379bb0812a89668",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "c62ff04d616fbc6d29ebbf1a9e81615b",
+    "url": "/images/pitsku.jpg"
+  },
+  {
+    "revision": "4bd029f2fb04d73caaee40e0a615d57d",
+    "url": "/images/taustista.jpg"
+  }
+];
